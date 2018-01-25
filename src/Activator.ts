193db55ed9fabe66ec94bdcd1595e2b36951458d
Robert Kania <cezaryrk@yahoo.de>
2018-01-25 23:33:24 +0100

    add server registry to activator

--- src/Activator.ts
+++ src/Activator.ts
@@ -1,4 +1,6 @@
-import {RuntimeLoader,Inject} from "typexs-base";
+import {RuntimeLoader,Inject,Config,TYPEXS_NAME,Container} from "typexs-base";
+import {ServerRegistry} from "./libs/server/ServerRegistry";
+import {C_SERVER} from "./types";
 
 
 export class Activator {
@@ -6,7 +8,14 @@ export class Activator {
   @Inject()
   loader: RuntimeLoader;
 
-  startup(){
+  async startup(){
+
+    let data = Config.get(C_SERVER, TYPEXS_NAME);
+    let serverRegistry = new ServerRegistry();
+    if(data){
+      await serverRegistry.load(data);
+      Container.set(ServerRegistry,serverRegistry);
+    }
 
   }
 
