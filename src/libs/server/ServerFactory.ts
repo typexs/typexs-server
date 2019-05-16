b7bc7fb7edb9dc1fd29a1cf8e9560800f8241c23
Robert Kania <cezaryrk@yahoo.de>
2019-05-16 16:47:32 +0200

    fix: cleanup imports

--- src/libs/server/ServerFactory.ts
+++ src/libs/server/ServerFactory.ts
@@ -1,5 +1,5 @@
 import * as _ from "lodash";
-import {Inject, Container, StringOrFunction, PlatformUtils, ClassLoader, Log} from "@typexs/base";
+import {ClassLoader, Container, Log, StringOrFunction} from "@typexs/base";
 import {ClassType} from "commons-schema-api";
 
 import {WebServer} from "../web/WebServer";
