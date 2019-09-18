00d82506638fcf1f52cc5ab6a9ccde970f7a4349
Robert Kania <cezaryrk@yahoo.de>
2019-09-18 12:25:11 +0200

    refactor: tslint

--- test/functional/controllers/context_group.spec.ts
+++ test/functional/controllers/context_group.spec.ts
@@ -1,8 +1,8 @@
-import {suite, test} from "mocha-typescript";
-import {ClassLoader, MetaArgs} from "@typexs/base";
-import {expect} from "chai";
-import {K_META_CONTEXT_ARGS} from "../../../src/libs/Constants";
-import {Helper} from "../../../src/libs/Helper";
+import {suite, test} from 'mocha-typescript';
+import {ClassLoader, MetaArgs} from '@typexs/base';
+import {expect} from 'chai';
+import {K_META_CONTEXT_ARGS} from '../../../src/libs/Constants';
+import {Helper} from '../../../src/libs/Helper';
 
 
 @suite('functional/controllers/context_group')
@@ -20,14 +20,14 @@ class Context_groupSpec {
 
   @test
   async 'grouping'() {
-    let classes = ClassLoader.importClassesFromDirectories([__dirname + '/fake_app/controllers/*']);
+    const classes = ClassLoader.importClassesFromDirectories([__dirname + '/fake_app/controllers/*']);
     expect(classes).to.have.length(3);
 
-    let grouped = MetaArgs.key(K_META_CONTEXT_ARGS);
+    const grouped = MetaArgs.key(K_META_CONTEXT_ARGS);
     expect(grouped).to.have.length(2);
-    let groups = Helper.resolveGroups(classes);
+    const groups = Helper.resolveGroups(classes);
     expect(Object.keys(groups)).to.deep.eq([ 'api', 'api2', 'default' ]);
-    for(let k in groups){
+    for (const k in groups) {
       expect(groups[k]).to.have.length(1);
     }
 
